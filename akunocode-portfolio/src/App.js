752db005f92ba754ae081d8a204c5b2a1d2f93a9
John Leo D. Echevaria <109233177+AkunoCode@users.noreply.github.com>
2023-06-18 21:30:31 +0800

    Test on the link

--- akunocode-portfolio/src/App.js
+++ akunocode-portfolio/src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './NavBar/NavBar'
 import Home from './Home/Home'
 
 function App() {
     // Routes
     return (
-        <BrowserRouter>
+        <HashRouter basename='/AkuNoCode-Portfolio'>
             <Routes>
                 <Route path='/' element={<NavBar />}>
                     <Route index element={<Home />} />
                     <Route path='*' element={<h1 id='NotFound'>404 Not Found</h1>} />
                 </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
